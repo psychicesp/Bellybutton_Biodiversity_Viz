cd8d53bfe8bdf643f94949d48ef87e65ce29be71
Eric S Penrose <ericspenrose@gmail.com>
2020-11-09 09:29:30 -0600

    Update app.js

---
+++ static/js/app.js
@@ -0,0 +1,22 @@
+// ------- samples.json format -------
+// samples.json {
+//     .names{
+//         ["ID#s in a list"]
+//     },
+//     .metadata{
+//         .id: "ID #",
+//         .ethnicity: "Patient Ethnicity",
+//         .gender: "Patient Gender",
+//         .age: "Patient Age in years.0",
+//         .location: "Where data was collected..?",
+//         .bbtype: "Bellybutton type:  Innie/Outie",
+//         .wfreq: "Frequency washing belly button per ..?",
+//         },
+//     .samples{
+//         .id: "ID #",
+//         .otu_ids: ["List of bacterial ID#s corresponding in position to the sample_values"],
+//         .sample_values: ["List of #CFUs? corresponding in position to the otu_ids"]
+//         .otu_labels: ["List of ';' separated strings containing ..?"]
+//     }
+// }
+// ---------- format END -------------
